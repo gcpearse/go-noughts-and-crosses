2beb999afb57c04729e1d89d890d99b89e36157c
gcpearse <glen.c.pearse@gmail.com>
2024-11-29 22:00:34 +0000

    Adds tests for isGameOver

--- game/game-over_test.go
+++ game/game-over_test.go
@@ -64,3 +64,32 @@ func TestIsDraw(t *testing.T) {
 		}
 	}
 }
+
+func TestIsGameOver(t *testing.T) {
+	tests := []struct {
+		board [3][3]string
+		want  bool
+	}{
+		{[3][3]string{
+			{"o", "o", "o"},
+			{"x", "x", "."},
+			{".", ".", "."},
+		}, true},
+		{[3][3]string{
+			{"o", "x", "o"},
+			{"o", "x", "x"},
+			{"x", "o", "o"},
+		}, true},
+		{[3][3]string{
+			{"o", ".", "o"},
+			{".", "x", "."},
+			{"x", ".", "o"},
+		}, false},
+	}
+
+	for _, test := range tests {
+		if got := isGameOver(test.board); got != test.want {
+			t.Errorf("got: %v, wanted: %v", got, test.want)
+		}
+	}
+}
